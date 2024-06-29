4ba3ef63ac00409d7e7397a99c7f73e91c787fe0
Lenrie13 <lenrie.ogutu13@gmail.com>
2024-06-29 20:46:35 +0300

    update arithmetics lab

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+// num1 and num2 multiplication result should be 62
+const num1 = 31;
+const num2 = 2;
 
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+// variable called random- generates and int>0
+const random = Math.floor(Math.random() * 10) + 1;
+
+console.log(random);
+
+//Variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4
+const num3 = 40;
+const num4 = 9;
+const mod = num3 % num4;
+
+console.log(mod);
+
+// Variable called max that finds the highest number in a set; the value returned should be 20.
+const max = Math.max(3, 17, 12, 20, 13, 9, 7);
+
+console.log(max);
 // Write your code here
